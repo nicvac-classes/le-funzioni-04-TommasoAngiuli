f71f895d89d8bd18518700c38220b42d5ab2175c
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2025-12-03 22:00:01 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,73 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+double addizione(double a, double b) 
+{
+    double somma;
+    somma = a + b;
+    return somma;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+double divisione(double a, double b) 
+{
+    double rapporto;
+    rapporto = a / b;
+    return rapporto;
+}
+
+double input() 
+{
+    double in, a;
+    do 
+    {
+        cout << "inserisci un valore positivo" << endl;
+        cin >> a;
+    } while (a < 0);
+    in = a;
+    return in;
+}
+
+double moltiplicazione(double a, double b) 
+{
+    double prodotto;
+    prodotto = a * b;
+    return prodotto;
+}
+
+double potenza(double a, double b) 
+{
+    double pot;
+    int i;
+    pot = 1;
+    for (i = 1; i <= b; i++) 
+    {
+        pot = pot * a;
+    }
+    return pot;
+}
+
+double sottrazione(double a, double b)
+{
+    double differenza;
+    differenza = a - b;
+    return differenza;
+}
+int main() 
+{
+    double a, b, somma, differenza, rapporto, prodotto, pot, in;
+    cout << "inserisci il primo valore" << endl;
+    a = input();
+    cout << "inserisci il secondo valore" << endl;
+    b = input();
+    somma = addizione(a, b);
+    cout << "la somma tra i due valori è " << somma << endl;
+    differenza = sottrazione(a, b);
+    cout << "la differenza tra i due valori è " << differenza << endl;
+    prodotto = moltiplicazione(a, b);
+    cout << "il prodotto tra i due valori è " << prodotto << endl;
+    rapporto = divisione(a, b);
+    cout << "il rapporto tra i due valori è " << rapporto << endl;
+    pot = potenza(a, b);
+    cout << "il primo valore elevato al secondo è " << pot << endl;
+    return 0;
+}
